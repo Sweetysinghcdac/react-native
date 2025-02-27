cd77e7accaa37fcbc9d663d5a4bd8f07c17355d7
Sweety <sweetysinghcdac@gmail.com>
2025-02-27 12:58:52 +0530

    bottom tab navigation code

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from "./screens/Home";
+import Status from "./screens/Status";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const { View, Text, TouchableOpacity } = require("react-native");
+
+
+
+const Tab = createBottomTabNavigator()
+
+// function App(){
+  // return(
+  //   <View>
+  //       <Text>Text xontainer</Text>
+  //       <Text>Text xontainer</Text>
+
+  //   </View>
+  // )
+// }
+
+
+
+const App = () =>{
+  return(
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+            headerShown: true,
+            headerStyle: {
+              backgroundColor: 'red'
+            },
+            tabBarStyle:{
+              backgroundColor:'powderblue'
+            }
+          }}
+      >
+
+        <Tab.Screen name="Home" component={Home}></Tab.Screen>
+        <Tab.Screen name="Status" component={Status}></Tab.Screen>
+
+
+      </Tab.Navigator>
+    </NavigationContainer>
+   
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screens/Status.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { PureComponent } from 'react'
+
+export class Status extends PureComponent {
+  render() {
+    return (
+      <View>
+        <Text>Status</Text>
+      </View>
+    )
+  }
+}
+
+export default Status
