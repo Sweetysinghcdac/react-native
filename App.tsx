6e9dcf444caabb8ddc60e3f97f6375ff1f26cf9a
Sweety <sweetysinghcdac@gmail.com>
2025-03-04 12:39:20 +0530

    @type/react: 18.0.8 added in package.json

--- App.tsx
+++ App.tsx
@@ -35,8 +35,22 @@ const App = () =>{
             }
           }}
       >
+      <Tab.Screen 
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View>
+              <Image 
+                source={require('.asset/image.png')}
+                style={{ width: 24, height: 24, tintColor: focused ? 'blue' : 'gray' }}
+              />
+              <Text style={{ color: focused ? 'blue' : 'gray' }}>Home</Text>
+            </View>
+          )
+        }}
+      />
 
-        <Tab.Screen name="Home" component={Home}></Tab.Screen>
         <Tab.Screen name="Status" component={Status}></Tab.Screen>
 
 
